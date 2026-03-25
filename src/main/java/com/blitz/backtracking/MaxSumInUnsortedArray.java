1729f19335a6c590d949b1413ab9452f52b2947f
dev-blitz <abhinab.duttagupta@gmail.com>
2026-03-25 20:58:47 +0530

    added Fibonacci implementation: * added method without recursion * added implementation with recursion   * added both optimised and unoptimised approaches for comparison

---
+++ src/main/java/com/blitz/backtracking/MaxSumInUnsortedArray.java
@@ -0,0 +1,13 @@
+package com.blitz.backtracking;
+
+public class MaxSumInUnsortedArray {
+  public static int maxSumInUnsortedArray(int[] arr) {
+    int current = arr[0];
+    int max = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+       current = arr[i] + current > arr[i] ? arr[i] + current : arr[i];
+       max = max > current ? max : current;
+    }
+    return max;
+  }
+}
---
+++ src/main/java/com/blitz/backtracking/without_recursion/Fibonacci.java
@@ -0,0 +1,93 @@
+package com.blitz.backtracking.without_recursion;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * this class provides the implementation of Fibonacci sequence
+ * without using recursions
+ * @author blitz
+ */
+public class Fibonacci {
+  
+  /**
+   * calculates the fibonacci without recursion;
+   * @param int num number whose fibonacci value 
+   * we want to calculate
+   * @return int the integer fibonacci value
+   */
+  public static int fibonacci(int num) {
+    int[] arr = new int[num + 1];
+    int first = 0;
+    int second = 1;
+    arr[0] = first;
+    arr[1] = second;
+    int fibonacci = 0;
+    for (int i = 2; i <= num; i++) {
+      fibonacci = first + second;
+      arr[i] = fibonacci;
+      first = second;
+      second = fibonacci;
+    }
+    return fibonacci;
+  }
+  
+  /**
+   * un-optimised fibonacci implementation
+   * @param int num whose fibonacci we want to calculate
+   * @return int fibonnaci value of the number
+   */
+  private static int fibRecUnOpt(int num) {
+    if (num == 0 || num == 1)
+      return num;
+    return fibRecUnOpt(num - 1) + fibRecUnOpt(num - 2);
+  }
+  
+  /**
+   * optimised recursive fibonnaci implementation
+   * @param int num whose value we want to calculate
+   * @return int fibonnaci value
+   */
+  private static int fibRecOpt(int num) {
+    return fibRecOpt(num, new HashMap<Integer, Integer>());
+  }
+
+  /**
+   * overloaded optimised fibonnaci implementation for reference
+   * @param int num whose fibonnaci value we want to calculate
+   * @param Map<Integer, Integer> map to retain the values calculated
+   * @return int fibonnaci value of the number
+   */
+  private static int fibRecOpt(int num, Map<Integer, Integer> map) {
+    if (map.containsKey(num))
+      return map.get(num);
+    else if (num == 0 || num == 1)
+      return num;
+    int result = fibRecOpt(num - 1, map) + fibRecOpt(num - 2, map);
+    map.put(num, result);
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int num = 5;
+    System.out.println("fibonacci of " + num + ": " + fibonacci(num));
+    num = 6;
+    System.out.println("fibonacci of " + num + ": " + fibonacci(num));
+    num = 21;
+    System.out.println("fibonacci of " + num + ": " + fibonacci(num));
+    System.out.println("OPTIMISED-RECUSION");
+    num = 5;
+    System.out.println("fibonacci of " + num + ": " +fibRecOpt(num));
+    num = 6;
+    System.out.println("fibonacci of " + num + ": " +fibRecOpt(num));
+    num = 21;
+    System.out.println("fibonacci of " + num + ": " +fibRecOpt(num));
+    System.out.println("UN-OPTIMISED-RECUSION");
+    System.out.println("fibonacci of " + num + ": " +fibRecUnOpt(num));
+    num = 6;
+    System.out.println("fibonacci of " + num + ": " +fibRecUnOpt(num));
+    num = 21;
+    System.out.println("fibonacci of " + num + ": " +fibRecUnOpt(num));
+    num = 5;
+  }
+}
