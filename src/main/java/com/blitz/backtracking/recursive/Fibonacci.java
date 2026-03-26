930b88686c281e74f0c31914e800a1f49e8bc3c8
dev-blitz <abhinab.duttagupta@gmail.com>
2026-03-26 13:53:47 +0530

    changed and shortened the Fibonacci implementation

--- src/main/java/com/blitz/backtracking/recursive/Fibonacci.java
+++ src/main/java/com/blitz/backtracking/recursive/Fibonacci.java
@@ -16,18 +16,11 @@ public class Fibonacci {
    */
   public static int fibonacci(int num) {
     int[] arr = new int[num + 1];
-    int first = 0;
-    int second = 1;
-    arr[0] = first;
-    arr[1] = second;
-    int fibonacci = 0;
-    for (int i = 2; i <= num; i++) {
-      fibonacci = first + second;
-      arr[i] = fibonacci;
-      first = second;
-      second = fibonacci;
-    }
-    return fibonacci;
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i <= num; i++)
+      arr[i] = arr[i - 1] + arr[i - 2];
+    return arr[arr.length - 1];
   }
   
   /**
